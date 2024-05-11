63aedef2e718d1c8a65878ac8260f1067daeaea9
chocoscoding <chocoscoding@gmail.com>
2024-05-11 14:25:00 +0100

    sitemap fix

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -7,34 +7,6 @@ import RootProvider from "./RootProvider";
 import NextTopLoader from "nextjs-toploader";
 const inter = Manrope({ subsets: ["latin"] });
 
-const metainfo = {
-  name: "Flexible QR",
-  description: "Create a QR code once and change its link or function anytime",
-  url: "https://flexibleqrcodes.vercel.app/",
-  image: "/meta/meta.png",
-  favicon: "/favicon.ico",
-};
-
-export const metadata: Metadata = {
-  icons: metainfo.favicon,
-  metadataBase: new URL(metainfo.url),
-  title: metainfo.name,
-  description: metainfo.description,
-  authors: {
-    name: metainfo.name,
-    url: metainfo.url,
-  },
-  creator: metainfo.name,
-  openGraph: {
-    type: "website",
-    url: metainfo.url,
-    title: metainfo.name,
-    description: metainfo.description,
-    images: metainfo.image,
-  },
-  keywords: ["changeable qr code", "dynamic qr code", "dynamic qr", "business", "qr codes", "QR CODES", "qr", "chocoscoding"],
-};
-
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,42 @@ import Script from "next/script";
 
 const inter = Manrope({ subsets: ["latin"] });
 
+const metainfo = {
+  name: "Flexible QR",
+  description: "Create a QR code once and change its link or function anytime",
+  url: "https://flexibleqrcodes.vercel.app/",
+  image: "/meta.png",
+  favicon: "/favicon.ico",
+};
+
 export const metadata: Metadata = {
-  title: "Flexible Qr",
-  description: "Create a QR code that changes in info but not in look",
+  icons: metainfo.favicon,
+  metadataBase: new URL(metainfo.url),
+  title: metainfo.name,
+  description: metainfo.description,
+  authors: {
+    name: metainfo.name,
+    url: metainfo.url,
+  },
+  creator: metainfo.name,
+  openGraph: {
+    type: "website",
+    url: metainfo.url,
+    title: metainfo.name,
+    description: metainfo.description,
+    images: metainfo.image,
+  },
+  keywords: ["changeable qr code", "dynamic qr code", "dynamic qr", "business", "qr codes", "QR CODES", "qr", "chocoscoding"],
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <Script strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6630162015653294" />
+      <Script
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6630162015653294"
+      />
     </html>
   );
 }
