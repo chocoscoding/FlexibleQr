0fd3f8732517e2dc5f6e1869ba592558d77cc090
chocoscoding <chocoscoding@gmail.com>
2024-11-17 16:17:55 +0100

    added privacy ploicy

---
+++ app/(main)/privacy-policy/page.tsx
@@ -0,0 +1,125 @@
+const PrivacyPolicy = () => {
+  return (
+    <div className="privacy-policy p-6 bg-gray-100 text-gray-800">
+      <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
+      <p className="mb-4">Effective date: 01/12/2023</p>
+
+      <h2 className="text-2xl font-semibold mb-2">1. Introduction</h2>
+      <p className="mb-4">
+        These Privacy Policy (“Privacy Policy”) apply to the use of the website and products provided by (hereinafter also referred as “we”
+        or “us”).
+      </p>
+      <p className="mb-4">
+        This Privacy Policy applies and has effect in respect of all services and other products, software, made available by us, as well as
+        any other online features relating to the website and its content (the “Service(s)”).
+      </p>
+      <p className="mb-4">If you have any questions or comments about this Privacy Policy, please contact us at chocoscoding@gmail.com</p>
+      <p className="mb-4">
+        We are committed to protecting and respecting your privacy. The Privacy Policy explains the basis on which personal information we
+        collect from you will be processed by us or on our behalf. Where we decide the purpose or means for which personal data you supply
+        through these Services is processed, we are the “controller”. Where you decide the purpose or means for which personal data you
+        supply through these Services is processed, you are the “controller”. We will comply with proper and applicable data protection
+        laws, including the General Data Protection Regulation 2016/679.
+      </p>
+      <p className="mb-4">
+        We encourage you to read this Privacy Policy carefully as it contains important information about the following:
+      </p>
+      <ul className="list-disc list-inside mb-4">
+        <li>What information we may collect about you;</li>
+        <li>How we will use the information we collect about you;</li>
+        <li>Whether we will disclose your details to anyone else; and</li>
+        <li>Your choices and rights regarding the personal information you have provided to us.</li>
+      </ul>
+      <p className="mb-4">
+        The Services may contain links to services owned and operated by third parties. We may also use some third-parties software or
+        products to provide you with the Service properly. If we do so and provide third-parties of any personal data you can be sure the
+        transfer is legal and secured. These third-party services may have their own privacy policies and we recommend that you review them.
+        They will govern the use of personal information that you submit or which is collected by cookies and other tracking technologies
+        whilst using these services. We do not accept any responsibility or liability for the privacy practices of such third party services
+        and your use of these is at your own risk.
+      </p>
+      <p className="mb-4">
+        We may make changes to this Privacy Policy in the future. You should check this page from time to time to ensure you are aware of
+        any changes. Where appropriate we may notify you of such changes.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">2. Payment and Card Processing</h2>
+      <p className="mb-4">
+        Also, We do not store any credit card information in server, all payments are processed by world leading payment gateway PayPal and
+        Stripe and our site is secured by SSL encryption.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">3. Information we may collect about you</h2>
+      <p className="mb-4">We collect and process the following information which may include your personal data.</p>
+      <p className="mb-4">
+        Your name, last name, and email address you supply to us and information provided by you when using the Service or website.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">4. Collecting, processing and using personal data</h2>
+      <p className="mb-4">
+        We only store and process your personal data when you have voluntarily supplied us with it such as by filling in a contact form or
+        signing up for the product purchase. Your personal data will only be disclosed or otherwise transmitted if this is necessary to
+        implement the contract, render our Services or you have given your prior consent.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">5. Why we collect information about you – purpose of processing</h2>
+      <p className="mb-4">
+        We will use information about you for delivering our Services to you under the terms of use agreed between us. The processing of
+        information in this way is necessary for us to provide you the Service properly and to ensure the features function properly so that
+        you have the best Service possible.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">6. Cookies and Web Beacons</h2>
+      <p className="mb-4">
+        We use cookies to make the use of our website easier for you. These cookies are used to store information including visitors’
+        preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users’
+        experience by customizing our web page content based on visitors’ browser type and/or other information. Cookies may be opt-out by
+        you and in case you do not accept cookies, this may lead to a limitation of functionality.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">7. Geo-location data</h2>
+      <p className="mb-4">
+        In case geo-location is opt-in by you, please be aware we may have access to your geo-location data as we collect and process IP
+        address of all devices using our Services. Collecting and processing your geo-location data refers to mobile devices as well as
+        computers.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">8. Data sharing</h2>
+      <p className="mb-4">
+        We will share your information (including personal data) with third parties only in the ways that are described in this Privacy
+        Policy.
+      </p>
+      <p className="mb-4">
+        Personnel, suppliers or subcontractors: We keep your information confidential, but may disclose it to our personnel, suppliers or
+        subcontractors insofar as it is reasonably necessary for the purposes set out in this Privacy Policy. However, this is on the basis
+        that they do not make independent use of the information, and have agreed to safeguard it.
+      </p>
+      <p className="mb-4">
+        Required by law: In addition, we may disclose your information to the extent that we are required to do so by law (which may include
+        to government bodies and law enforcement agencies); in connection with any legal proceedings or prospective legal proceedings; and
+        in order to establish, exercise or defend our legal rights.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">9. Data transmission</h2>
+      <p className="mb-4">
+        Your data is transmitted safely by using encryption. We secure our website and other systems with technical and organizational
+        measures against the loss, destruction, access, change or dissemination of your data by unauthorized persons.
+      </p>
+
+      <h2 className="text-2xl font-semibold mb-2">10. Your rights and delete your data</h2>
+      <p className="mb-4">You have the following rights over the way your personal data are processed.</p>
+      <p className="mb-4">To make a request, please let us know by sending an email to chocoscoding@gmail.com.</p>
+      <p className="mb-4">You can also delete your data yourself without our approval or actions.</p>
+
+      <h2 className="text-2xl font-semibold mb-2">11. Changes to this privacy policy</h2>
+      <p className="mb-4">
+        This Privacy Policy may be updated from time to time. We encourage you to review this website for the latest information on our
+        privacy practices. If there are any material changes to this Privacy Policy, you will be notified by posting a notice on the website
+        prior to the change becoming effective. If you do not accept any changes made to this Privacy Policy, please discontinue the use of
+        the website and the Services.
+      </p>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
